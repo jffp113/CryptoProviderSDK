9a639fcf57ea09968bbb4a20ef77eeba708a7120
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2021-05-21 11:31:08 +0100

    Bug correction on optimisitic algorithm and Optimisitic and Pessimistic were swapped in trsa

--- example/handlers/trsa/trsaOptimistic.go
+++ example/handlers/trsa/trsaOptimistic.go
@@ -1,23 +1,51 @@
 package trsa
 
 import (
+	"crypto/rsa"
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"github.com/jffp113/CryptoProviderSDK/crypto"
+	"github.com/jffp113/go-util/algorithms/twiddle"
 	"github.com/niclabs/tcrsa"
 )
 
 const OptimisticScheme = "TRSA%vOptimistic"
 
+
+
 func aggregateOptimistic(sigShares tcrsa.SigShareList,digest []byte,pub pubKey, t, n int) (signature []byte, err error){
+	if len(sigShares) < t {
+		return nil, errors.New("not enough signatures")
+	}
+
 	docHash := sha256.Sum256(digest)
 	docPKCS1, err := tcrsa.PrepareDocumentHash(pub.Meta.PublicKey.Size(), HashType, docHash[:])
 
-	if err != nil {
-		return
+	tw := twiddle.New(t, len(sigShares))
+
+	for b := tw.Next(); b != nil; b = tw.Next() {
+		var perm []*tcrsa.SigShare
+
+		for i, c := range b {
+			if c {
+				perm = append(perm, sigShares[i])
+			}
+		}
+
+		 sig,err := tcrsa.SigShareList(perm).Join(docPKCS1, pub.Meta)
+		 if err != nil {
+		 	continue
+		 }
+
+		err = rsa.VerifyPKCS1v15(pub.Meta.PublicKey, HashType, docHash[:], sig)
+
+		if err == nil {
+			return sig,nil
+		}
+
 	}
-	valid := getValidShares(sigShares,docPKCS1,pub)
-	return valid.Join(docPKCS1, pub.Meta)
+	return nil, errors.New("no valid combination found")
 }
 
 func NewOptimisticTRSA(size int) crypto.SignerVerifierAggregator {
--- example/handlers/trsa/trsaPessimistic.go
+++ example/handlers/trsa/trsaPessimistic.go
@@ -1,45 +1,35 @@
 package trsa
 
 import (
+	"crypto/rsa"
 	"crypto/sha256"
-	"errors"
 	"fmt"
 	"github.com/jffp113/CryptoProviderSDK/crypto"
-	"github.com/jffp113/go-util/algorithms/twiddle"
 	"github.com/niclabs/tcrsa"
 )
 
 const PessimisticScheme = "TRSA%vPessimistic"
 
 func aggregatePessimistic(sigShares tcrsa.SigShareList,digest []byte,pub pubKey, t, n int) (signature []byte, err error){
-	if len(sigShares) < t {
-		return nil, errors.New("not enough signatures")
-	}
-
 	docHash := sha256.Sum256(digest)
 	docPKCS1, err := tcrsa.PrepareDocumentHash(pub.Meta.PublicKey.Size(), HashType, docHash[:])
 
-	tw := twiddle.New(t, len(sigShares))
-
-	for b := tw.Next(); b != nil; b = tw.Next() {
-		var perm []*tcrsa.SigShare
-
-		for i, c := range b {
-			if c {
-				perm = append(perm, sigShares[i])
-			}
-		}
-
-		valid := getValidShares(perm,docPKCS1,pub)
-
-		if len(valid) >= t {
-			return valid.Join(docPKCS1, pub.Meta)
-		}
-
+	if err != nil {
+		return
 	}
-	return nil, errors.New("no valid combination found")
-}
+	valid := getValidShares(sigShares,docPKCS1,pub)
 
+
+	sig,err := valid.Join(docPKCS1, pub.Meta)
+
+	if err != nil {
+		return []byte{},err
+	}
+
+	err = rsa.VerifyPKCS1v15(pub.Meta.PublicKey, HashType, docHash[:], sig)
+
+	return sig,err
+}
 
 func NewPessimisticTRSA(size int) crypto.SignerVerifierAggregator {
 	return &trsa{
